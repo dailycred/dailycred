03cb2c592ff4285c90ff03e18d9456a385e500b4
Hank Stoever <hstove@gmail.com>
2012-09-06 19:27:17 -0700

    removed tester app

--- examples/tester/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tester::Application.config.secret_token = 'd64accc82c85ed91d1361b80ae71dfefd1405407d9a5bc2134c5fbe895829c7ab5955a966ac717626fa314ab2aa0f2d09d87fd619c1f7aa57d705ee9aac7b0d5'
